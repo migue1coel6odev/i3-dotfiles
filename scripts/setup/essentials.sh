c59f4fde5651fdae12f87232e4226260410b62b7
Miguel Coelho <miname>
2024-10-28 20:52:15 +0000

    Added gdb & pwndbg to essentials

--- scripts/setup/essentials.sh
+++ scripts/setup/essentials.sh
@@ -1,3 +1,3 @@
-sudo pacman -S stow pavucontrol playerctl github-cli neovim neofetch tmux vi python curl tree man man-db less firefox galculator
+sudo pacman -S stow pavucontrol playerctl github-cli neovim neofetch tmux vi python curl tree man man-db less firefox galculator gdb pwndbg
 sudo pacman -S qt5-quickcontrols2 qt5-graphicaleffects qt5-svg
 sudo pacman -S dkms linux-headers
